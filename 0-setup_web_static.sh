df5d21b24ce682e68e99a5bdf4ca5cfbe5e3227a
James Ononiwu <jamesononiwu@gmail.com>
2023-05-04 05:50:56 -0700

    set up servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
-# sets up Airbnb static_web files amd folders
-sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo apt-get install -y nginx
-mkdir -p /data/web_static/
-mkdir -p /data/web_static/releases/test/
-mkdir /data/web_static/shared/
-echo "Airbnb Project" | sudo tee /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test /data/web_static/current
-chown ubuntu:ubuntu -hR /data/
+# sets up the web servers for the deployment of web_static
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data/
 sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx restart
+sudo service nginx start
