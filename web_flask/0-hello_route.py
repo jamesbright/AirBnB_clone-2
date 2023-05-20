9848e18b6e85455951d192b09261a5871e7618e6
jamesbright <jamesononiwu@gmail.com>
2023-05-20 16:51:49 -0700

    added port number

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,7 +6,12 @@ app = Flask(__name__)
 # home route that returns a text when accessed
 @app.route('/', strict_slashes=False)
 def hello_route():
+    """Prints text
+    Returns:
+        text
+    """
+
     return "Hello HBNB!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
